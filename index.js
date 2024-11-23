59dec8da6c9a091c09a33e49557560e6b75c6a24
veyselustuntas <veyselustuntas@hotmail.com>
2024-11-23 15:42:28 +0300

    feat: node.js ile file system kullanimi

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-console.log("merhaba");
-
 var http = require("http");
+var fs = require("fs");
 
-var server = http.createServer(function(req, res) {
-    console.log(req.url);
-
-    res.end();
+var server = http.createServer(function (req, res) {
+    if (req.url == "/") {
+        fs.readFile("index.html", (err, html) => {
+            res.write(html);
+            res.end();
+        });
+    }
+    else if (req.url == "/products") {
+        fs.readFile("product.html", (err, html) => {
+            res.write(html);
+            res.end();
+        });
+    }
+    else {
+        fs.readFile("not-found.html", function (err, html) {
+            res.write(html);
+            res.end();
+        });
+    }
 });
 
-server.listen(3000,() => {
+server.listen(3000, () => {
     console.log("node.js server at port 3000");
 });
 

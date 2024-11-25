197c704343841d323f2cd44401a9615126874dd0
veyselustuntas <veyselustuntas@hotmail.com>
2024-11-25 13:04:17 +0300

    feat: proxy get kullanimi

--- index.js
+++ index.js
@@ -33,10 +33,28 @@ var server = http.createServer(function (req, res) {
             }
         };
 
+        
+        let handler5= {
+            get(target, prop, receiver) {
+                if(prop == "message2"){
+                    return new Error("Hata Erişilemez");
+                }
+                return Reflect.get(...arguments);
+            }
+        };
+
+        let handler6= {
+            get(target, prop, receiver) {
+                return target[prop] || "default value";
+            }
+        };
+
         let proxy = new Proxy(target, handler);
         let proxy2 = new Proxy(target, handler2);
         let proxy3 = new Proxy(target, handler3);
         let proxy4 = new Proxy(target, handler4);
+        let proxy5 = new Proxy(target, handler5);
+        let proxy6 = new Proxy(target, handler6);
 
         console.log("handler1")
         console.log(proxy.message1); // hello
@@ -56,6 +74,18 @@ var server = http.createServer(function (req, res) {
         console.log("handler4")
         console.log(proxy4.message1); // hello
         console.log(proxy4.message2); // world
+        console.log("\n");
+
+        console.log("handler5")
+        console.log(proxy5.message1); // hello
+        console.log(proxy5.message2); // Hata Erişilemez.
+        console.log("\n");
+
+        
+        console.log("handler6")
+        console.log(proxy6.message1); // hello
+        console.log(proxy6.message2); // everyone
+        console.log(proxy6.unknown); // default value
         console.log("-----------");
 
         res.end();
